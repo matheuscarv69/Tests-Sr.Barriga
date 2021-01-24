bb802b22f6e9a9733038950d788ca2b296d7f34c
matheuscarv69 <matheus9126@gmail.com>
2021-01-24 00:32:52 -0400

    :wrench: build: Strategy implemented to accelerate SuiteTests

--- src/main/java/core/Properties.java
+++ src/main/java/core/Properties.java
@@ -2,9 +2,9 @@ package core;
 
 public class Properties {
 
-    /** Propriedade usada para não fechar a execução do WebDriver -> BaseTest.class**/
+    /** Propriedade usada para não fechar a execução do WebDriver -> BaseTest.class **/
     /** OBS: NÃO RECOMENDADO, USADO APENAS FINS DIDÁTICOS **/
-    public static boolean CLOSE_BROWSER = true;
+    public static boolean CLOSE_BROWSER = false;
 
     /** Informe o Browser que deseja executar os testes **/
     public static Browsers browsers = Browsers.CHROME;
--- src/main/java/suites/SuiteTests.java
+++ src/main/java/suites/SuiteTests.java
@@ -1,8 +1,14 @@
 package suites;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import pages.LoginPage;
 import tests.*;
+
+import static core.DriverFactory.killDriver;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -15,5 +21,32 @@ import tests.*;
 })
 
 public class SuiteTests {
+
+
+    private static LoginPage loginPage = new LoginPage();
+    /**
+     * Aqui nesse @BeforeClass esta o metodo de login da pagina
+     * foi colocado aqui para acelerar a execucao dos testes, dessa forma,
+     * os testes nao voltam a tela de login, isso so eh feito uma vez durante
+     * a execucao da suite.
+     *
+     * Tambem visando acelerar a execucao dos testes, a propriedade CLOSE_BROWSER
+     * da classe Properties esta setada como false, assim o driver nao ira
+     * fechar apos finalizar um teste. BaseTest -> finalizerWebDriver
+     * **/
+    @BeforeClass
+    public static void initializer() {
+        loginPage.accessHomePage();
+
+        loginPage.setEmail("matheus69@gmail.com");
+        loginPage.setPassword("1111");
+
+        loginPage.clickButtonEnter();
+    }
+
+    @AfterClass
+    public static void finalizer() {
+        killDriver();
+    }
 
 }
